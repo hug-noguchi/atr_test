eca0e930da8fc7d51ac919cad64096a39122ea85
keito noguchi <noguchi@hug-inc.co.jp>
2025-07-03 21:26:58 +0900

    テキスト修正、リンク先変更

--- footer.php
+++ footer.php
@@ -17,16 +17,16 @@
         </p>
 
         <ul class="f_nav">
-            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><span>ホーム</span></a>｜</li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><span>最新記事</span></a>｜</li>
             <li><a href="<?php echo home_url(); ?>/knowledge/"><span>老人ホームのきほん</span></a>｜</li>
             <li><a href="<?php echo home_url(); ?>/qa/"><span>お悩みQ&A</span></a>｜</li>
             <li><a href="<?php echo home_url(); ?>/preparation/"><span>準備ガイド</span></a>｜</li>
             <li><a href="<?php echo home_url(); ?>/guide/"><span>介護のあれこれ</span></a>｜</li>
             <li><a href="<?php echo home_url(); ?>/healthcare/"><span>ヘルスケア(雑学・エンタメ)</span></a>｜</li>
 
-          <li><a href="https://www.bellco.co.jp/policy" atarget="_blank">プライバシーポリシー</a>｜</li>
-          <li><a href="https://www.bellco.co.jp/company" target="_blank">運営会社</a>｜</li>
-          <li><a href="https://www.bellco.co.jp/anatarasiku/" target="_blank">お問い合わせ</a></li>
+          <li><a href="https://www.bellco.co.jp/anatarasiku/privacy.html" atarget="_blank">プライバシーポリシー</a>｜</li>
+          <li><a href="https://www.bellco.co.jp/anatarasiku/">運営会社</a>｜</li>
+          <li><a href="https://www.bellco.co.jp/anatarasiku/">老人ホーム選びの無料サポートサービス</a></li>
         </ul>
         <!--<p id="f_bnr"><a href="https://test.gojyokuru.net/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/f_bnr.png" alt="ごじょクル">互助会のポータルサイトごじょクル</a></p>-->
         <p class="copyright">copyright © (株) ベルコ ライフサポートプランニング , all rights reserved.</p>
